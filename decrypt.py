bc5086ff00358a369fbccc294b351705710ad6a1
Irakli <i@irak.li>
2017-07-01 22:14:37 +0400

    Fixes

--- decrypt.py
+++ decrypt.py
@@ -11,7 +11,6 @@ def chunks(l, n):
 def remove_padding(block):
     val = block[-1]
     res = block[:-val]
-    padding = block[-val:]
     return res
 
 
@@ -28,38 +27,42 @@ def get_response(p, n):
     return Oracle_Send(p, n) == 1
 
 
+def get_pad(block, cur, index, size):
+    pad = -sys.maxint + 1
+    for i in xrange(0, 255):
+        block[index] = i
+        if get_response(block + cur, 2):
+            if index == size - 1:
+                temp = block[size - 2]
+                block[size - 2] += 1
+                block[size - 2] %= 256
+                if get_response(block + cur, 2):
+                    pad = i
+                    block[size - 2] = temp
+                    return pad
+                block[size - 2] = temp
+            else:
+                pad = i
+                return pad
+    return pad
+
+
 def decrypt(prev, cur, size):
     s = []
     for x in xrange(size):
         s.append(-sys.maxint + 1)
 
     for i in reversed(range(size)):
-        prev_b = prev[:]
+        prev_b = list(prev)
 
         for j in xrange(i + 1, size):
             prev_b[j] = prev[j] ^ s[j] ^ (size - i)
 
-        pad = -1
-        for k in xrange(0, 255):
-            prev_b[i] = k
-            if get_response(prev_b + cur, 2):
-                if i == size - 1:
-                    temp = prev_b[size - 2]
-                    prev_b[size - 2] += 1
-                    prev_b[size - 2] %= 256
-                    if get_response(prev_b + cur, 2):
-                        pad = k
-                        prev_b[size - 2] = temp
-                        break
-                    prev_b[size - 2] = temp
-                else:
-                    pad = k
-                    break
+        pad = get_pad(prev_b, cur, i, size)
 
-        s[i] = (size - i) ^ pad ^ prev[i]
+        s[i] = prev[i] ^ (size - i) ^ pad
         print s
     return s
-
 
 
 #############################################
@@ -84,9 +87,7 @@ result = list()
 for first, second in itertools.izip(clist, clist[1:]):
     result.append(decrypt(first, second, 16))
 
-
 result = cleanup(result)
 print result
-
 
 Oracle_Disconnect()
